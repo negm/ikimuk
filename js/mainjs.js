7408cd247eca4fab505a18019b8ae9951fa503d0
Hussein Negm <husseinngm@gmail.com>
2012-12-11 13:36:04 +0200

    Beta launched Dec10 fadeout problem

--- js/mainjs.js
+++ js/mainjs.js
@@ -112,7 +112,9 @@ return false;
 }
 //////////////////GA
  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-35526185-1']);
+  _gaq.push(['_setAccount', 'UA-36448861-1']);
+  _gaq.push(['_setDomainName', 'ikimuk.com']);
+  _gaq.push(['_setAllowLinker', true]);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -120,6 +122,8 @@ return false;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
+
   
 
 ///////////////////////////////////////upload & submit
