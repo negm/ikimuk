9e208222863c38aa822a4ae5aa6eb3fb325064a4
Hussein Negm <husseinngm@gmail.com>
2013-01-08 00:44:18 +0200

    third competition

--- block/listing_home.php
+++ block/listing_home.php
@@ -19,8 +19,8 @@ unset($_SESSION["size"]);
 echo '<div class="container compHeader center"><div class="row"><br><img  class="span12" src="/img/header_steps_ikimuk.png" alt="steps header ikimuk"/></div></div>';
 echo '<div class="container center">';
 echo '<h2 class= "centert ogcomp">ONGOING COMPETITION</h2>';
-echo '<div class="container compHeader center"><div class="row"><img  class="span12" src="/img/header_comp_ikimuk.jpg" alt="competition header ikimuk"/></div></div>';
-echo '<div class="container center"><b class="tlblue tlarge">OUR SECOND COMPETITION </b> <b class="tpink tmedium">  (ends on 07/01/2013</b>)<div class="lineb"></div><br>';
+echo '<div class="container compHeader center"><div class="row"><img  class="span12" src="/img/header_comp_ikimuk_twisted.jpg" alt="competition header ikimuk"/></div></div>';
+echo '<div class="container center"><b class="tlblue tlarge">OUR THIRD COMPETITION </b> <b class="tpink tmedium">  (ends on 21/01/2013</b>)<div class="lineb"></div><br>';
 $count = 0;
 while($row= mysqli_fetch_assoc($product->database->result))
 {   $daysLeft = floor((strtotime($row["end_date"]) - time())/(60*60*24));
