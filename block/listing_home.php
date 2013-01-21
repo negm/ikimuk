884b45677ed7917f34c4d2b7d2d7d8b6ddc7252f
negmhu <negmhu@10.112.15.172>
2013-01-21 23:50:18 +0200

    Zoombies competition

--- block/listing_home.php
+++ block/listing_home.php
@@ -19,8 +19,8 @@ unset($_SESSION["size"]);
 echo '<div class="container compHeader center"><div class="row"><br><img  class="span12" src="/img/header_steps_ikimuk.png" alt="steps header ikimuk"/></div></div>';
 echo '<div class="container center">';
 echo '<h2 class= "centert ogcomp">ONGOING COMPETITION</h2>';
-echo '<div class="container compHeader center"><div class="row"><img  class="span12" src="/img/header_comp_ikimuk_twisted.jpg" alt="competition header ikimuk"/></div></div>';
-echo '<div class="container center"><b class="tlblue tlarge">OUR THIRD COMPETITION </b> <b class="tpink tmedium">  (ends on 21/01/2013</b>)<div class="lineb"></div><br>';
+echo '<div class="container compHeader center"><div class="row"><img  class="span12" src="https://s3.amazonaws.com/competition-header/Header_Preorder_Zombie.png" alt="competition header ikimuk"/></div></div>';
+echo '<div class="container center"><b class="tlblue tlarge">OUR FOURTH COMPETITION </b> <b class="tpink tmedium">  (ends on 04/02/2013</b>)<div class="lineb"></div><br>';
 $count = 0;
 while($row= mysqli_fetch_assoc($product->database->result))
 {   $daysLeft = floor((strtotime($row["end_date"]) - time())/(60*60*24));
