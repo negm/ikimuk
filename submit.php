a0d7fcabacedc1391015f85e8f5faf69ce39fcc3
Hussein Negm <husseinngm@gmail.com>
2013-02-08 11:58:00 +0200

    submit

--- submit.php
+++ submit.php
@@ -43,82 +43,219 @@ else{
 });
 });</script>
 <?php
-include $_SERVER["DOCUMENT_ROOT"]."block/top_area.php";
-include $_SERVER["DOCUMENT_ROOT"]."block/breadcrumb.php";
+include $_SERVER["DOCUMENT_ROOT"]."/block/top_area.php";
+//include $_SERVER["DOCUMENT_ROOT"]."block/breadcrumb.php";
 ?>
-<div class="container">
-    <div class="row">
-        
-<div class="span3 tlblue tunderline"><br> <br><a href="../submit-intro.php">Back to submission guidelines</a></div>    
-<div class="preSummary span6 ">
-    
+<div class="body">
+             <div class="body_content">
+                 
+                                 
+                 <!--Start of Cart section-->
+                 <div class="cart_section">
+                     <div class="cart_content">
+                         <div class="cart_icon"></div>
+                         <div class="cart_details">
+                             CART(<span class="cart_count">0</span>)
+                         </div>
+                     </div>
+                 </div>
+                  <!--end of Cart section-->
+                  
+                  
+                  
+                  
+                  <div class="submit_design">
+                      
+                   <div class="design_header">
+                     SUBMIT YOUR DESIGN
+                  </div>
+                  
+                      
+                      <!--Start of Choose Competition-->
+                      <div class="choose_competition">
+                        <div class="std_block">
+                              
+                              <!--Start of cart table header-->
+                              <div class="std_block_header">
+                                  <div class="header_content">
+                                      1. Choose Competition
+                                  </div>
+                              </div>
+                                <!--End of cart table header-->
+                           
+                                  <!--Start of submit body-->
+                              <div class="std_block_body type_body">
+                                  <div class="type_select">
+                                  <input type="radio" name="competition_type" value="zombie" checked="checked"/>
+                                  For the Love Of Zombies Submit before 21/01/2013
+                                  </div>
+                                  <div class="type_select">
+                                  <input type="radio" name="competition_type" value="hakwaji"/>
+                                  HAKWAJI submit before 01/02/2013
+                                  </div>
+                              </div>
+                                    <!--End of submit body-->
+                          </div> 
+                      </div>
+                      <!--End of Choose Competition-->
+                      
+                      
+                      <!--Start of Add Images-->
+                       <div class="add_images">
+                        <div class="std_block">
+                              
+                              <!--Start of cart table header-->
+                              <div class="std_block_header">
+                                  <div class="header_content">
+                                      2. Add Images
+                                  </div>
+                              </div>
+                                <!--End of cart table header-->
+                           
+                                  <!--Start of submit body-->
+                              <div class="std_block_body">
+                                  <div class="upload_body_content">
+                                     <span class="upload_info"> Choose the file(s) to display your design. Submit a JPG version for your design (RGB mode not
+                                      CMYK), 300 px wide x 450 px tall. Maximum file size 250 KB.</span>
+                                   <ul id="files" class="thumb"></ul>
+                                   <div class="upload_holder">
+                                    <input type="button" id="upload" value="UPLOAD FILE" style=""/>
+                                   </div>
+                                      
+                                      </div>
+                                      
+                              </div>
+                                    <!--End of submit body-->
+                          </div> 
+                      </div>
+                      <!--End of Add Images-->
+                      
+                     
+                      <!--Start of Design Info-->
+                  <div class="design_info">
+                        <div class="std_block block_expandable">
+                              
+                              <!--Start of cart table header-->
+                              <div class="std_block_header">
+                                  <div class="header_content">
+                                      3. Design Info
+                                  </div>
+                              </div>
+                                <!--End of cart table header-->
+                           
+                                  <!--Start of submit body-->
+                              <div class="std_block_body info_body">
+                                 
+                                      <div class="line_info">   
+                                      <div class="line_header">Design Title</div>
+                                      <div class="line_input">
+                                            <input type="text" class="round_corners" name="design_title"/>
+                                      </div>
+                                      <div class="line_error"></div>
+                                      </div>
+                                      
+                                  
+                                      <div class="line_info">   
+                                      <div class="line_header">About your design</div>
+                                      <div class="line_input">
+                                          <textarea  class="round_corners" name="design_details"></textarea>
+                                      </div>
+                                      <div class="line_error"></div>
+                                      </div>
+                                  
+                                   
+                              </div>
+                                    <!--End of submit body-->
+                          </div> 
+                      </div> 
+                      <!--End of Design Info-->
+                      
+                    
+                      
+                   <!--Start of Self Info-->   
+                <div class="self_info">
+                        <div class="std_block block_expandable">
+                              
+                              <!--Start of cart table header-->
+                              <div class="std_block_header">
+                                  <div class="header_content">
+                                      4. Your Info
+                                  </div>
+                              </div>
+                                <!--End of cart table header-->
+                           
+                                  <!--Start of submit body-->
+                              <div class="std_block_body self_info_body">
+                                 
+                                      <div class="line_info">   
+                                      <div class="line_header">City</div>
+                                      <div class="line_input">
+                                            <input type="text" class="round_corners" name="city"/>
+                                      </div>
+                                      <div class="line_error"></div>
+                                      </div>
+                                      
+                                  
+                                     <div class="line_info">   
+                                      <div class="line_header">Website, Blog Or Portfolio</div>
+                                      <div class="line_input">
+                                            <input type="text" class="round_corners" name="website_blog_1"/>
+                                      </div>
+                                      <div class="line_error"></div>
+                                      </div>
+                                  
+                                  
+                                  <div class="line_info">   
+                                      <div class="line_header">Website, Blog Or Portfolio</div>
+                                      <div class="line_input">
+                                           <input type="text" class="round_corners" name="website_blog_2"/>
+                                      </div>
+                                      <div class="line_error"></div>
+                                      </div>
+                                  
+                                   
+                              </div>
+                                    <!--End of submit body-->
+                          </div> 
+                      </div>
+                      <!--End of self Info-->
+                      
+                  
+                      <!--Start of agreeement Section-->
+                    <div class="agreement_submit_section">
+                        
+                        
+                      <div class="agreement">
+                             <div class="terms_conditions">
+                                 <input type="checkbox" name="agree"/>
+                                <span>I agree on ikimuk's </span>
+                                 <a href="#">Terms & Conditions</a>
+                             </div>
+                             <div class="line_error"></div>
+                      </div>
+                         
+                         <div class="newsletter">
+                             <input type="checkbox" name="subscribe"/>
+                             <span>Keep me in the loop, sign me up for your newsletter</span>
+                         </div>
 
-<h1 class="preTitle">Submit your design</h1>
-<div class ="offset1 span4">
-<form id="submitDesign"  method="post" action="" class="">
+                                         
+                           <div class="submit_personal_design">
+                               <input type="button" name="submit_design" value="SUBMIT YOUR DESIGN" />
+                           </div>
+          
+                     </div>      
+                      <!--End Of agreement section-->
 
-<h3 class="tlarge tnheight">ADD IMAGES</h3>
-<div class="line"></div>
-<p>Choose the file(s) to display your design. Submit a JPG version for your design
-    (RGB mode not CMYK), 300 px wide x 450 px tall. Maximum file size 250 KB.</p>
-<ul id="files" class ="thumb"></ul>
-<p class="hidden" id="img_g">You forgot to upload your design</p>
-<p class="hidden" id="img_size_g">The file is larger than 250k</p>
-<div id="upload" class="btn btn-primary"><span>Upload File<span></div><span id="status" ></span><br/>
-<br>
-<h3 class="tlarge tnheight">DESIGN INFO</h3>
-<div class="line"></div><br>
-<p>Design title</p>
-<p class="hidden" id="title_g">Please choose a title!</p>
-<input type="text" name="design_title" id="design_title" class="span4"/>
-<p>About your design</p>
-<textarea class="span4" rows="6" name="comment" id="comment" placeholder="Tell us a little about what inspired you"></textarea>
-<p class="hidden" id="agreement_g"><small> You should read and agree on the terms</small></p>
-<label class="checkbox" >
-    <input id="agreement" name="agreement" class="" type="checkbox" value="1" /> 
-    I agree to ikimuk's <a href="#myModal" role="button" style="color:#44c6e3" data-toggle="modal">Terms & Conditions</a>
-</label>
-<label class="checkbox" >
-    <input id="newsletter" name="newsletter" class="" type="checkbox" value="1" /> 
-    Keep me in the loop, sign me up for your newsletter 
-</label>
-
-<input type="hidden" name="img_url" id="img_url"/>
- <br><br>
-<a id="submit_design" class="subButton span4 nomargin" >Submit your design</a>
-</form>
-</div>
-</div>
-<div id="orderComplete" class="span4 hidden">
-        <div class="preTitle">Submission complete</div>
-        Thank you for submitting <span id="title_msg" class="tlblue"></span>! We will update you soon.<br/>
-        Until then, <a href="index.php" style="color:#44c6e3">browse our other designs</a>
-        
-</div>
-<div id="submitFailed" class="span4 hidden">
-        <div class="txlarge">Submission FAILED!</div>
-        Thank you for submitting <span id="title_msg" class="tlblue"></span>! However, we weren't able to complete your submission<br/>
-        Please try again in a while. Until then, <a href="index.php" class="tlblue">browse our other designs</a>
-        
-</div>
-
-</div>
-</div>
-<!-- Modal -->
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">TERMS AND CONDITIONS FOR SUBMISSION OF DESIGNS</h3>
-  </div>
-  <div class="modal-body">
-    <div class="span16" style="font-size:12; color:#4c4c4c;padding-bottom:10;">
-        <?php include  $_SERVER["DOCUMENT_ROOT"].'/block/SubmissionTerms';?>
-    </div>
-  </div>
-  <div class="modal-footer">
-    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-  </div>
-</div><br>
+                  </div>
+                 <!--End of submit design-->
+   
+             </div>    
+             
+             <!--End of body content-->
+            
+        </div>
+        <!--End of body-->
 <?php
-include $_SERVER["DOCUMENT_ROOT"].'block/footer.php';
+include $_SERVER["DOCUMENT_ROOT"].'/block/footer.php';
 ?>
