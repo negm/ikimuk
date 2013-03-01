8820a1e695cca010e72e5d0bf299bd2931c8e27b
root <root@li579-37.members.linode.com>
2013-03-01 05:05:17 +0000

    added the about us page

---
+++ about-us.php
@@ -0,0 +1,44 @@
+<?php
+        
+include $_SERVER["DOCUMENT_ROOT"]."/block/header.php";
+
+include $_SERVER["DOCUMENT_ROOT"]."/block/top_area.php";
+ ?>
+<div class="body">
+    <div class="body_content aboutus">
+<div class="page_header">
+	THIS IS ikimuk
+</div>
+
+<div style="margin-top:15px">
+<img src="/img/aboutus.gif" />
+</div>
+<p class="blue_part">
+	If you're looking for beautifully designed, head-turning t-shirts, you've come to the right place. We sell the kind of tees that have a personality of their own.
+</p>
+
+<p class="blue_part">
+	And, if you're a creative, you can even design our stylish tees and make money. You'll love seeing people wearing your art. Guaranteed. 
+</p>
+
+
+<div class="quote" style="float:left">
+	<a href="http://www.dailystar.com.lb/Culture/Lifestyle/2013/Feb-14/206371-helping-design-dreams-become-fashion-reality.ashx#axzz2KrEVliH8" target="_blank">	
+  <div><i>"ikimuk is helping design dreams become fashion reality"</i></div>
+  <div style="float:right"><b>-The Daily Star</b></div>
+	</a>
+</div>
+
+<div class="quote" style="float:right">
+	<a href="http://ginosblog.com/?s=ikimuk" target="_blank">
+  <div><i>"Awesome, affordable and crowd-sourced fashion"</i></div>
+  <div style="float:right"><b>-Gino’s Blog</b></div>
+	</a>
+</div>
+
+<div class="sq"><a href="http://seeqnce.com" target="_blank"><img src="/img/seeqnce.png"/></a></div>
+</div>
+</div>
+<?
+include $_SERVER["DOCUMENT_ROOT"].'/block/footer.php';
+?>
